bee22c73d21743aa0913bead88c6870d3b7b9a35
pdvnny <pdunn91@gmail.com>
2022-10-31 23:59:36 -0400

    uploading additional data and reformating reo

--- scratch_scripts/test_file_save.py
+++ scratch_scripts/test_file_save.py
@@ -2,4 +2,4 @@ import osmdata as osm
 
 if __name__ == "__main__":
     traces = osm.parse_gps_traces("gps_data_by_city/temp.gpx", True)
-    osm.save_gps_traces("gps_data_by_city", "pittsburgh", traces)
+    osm.save_gps_traces("../gps_data_by_city", "pittsburgh", traces)
